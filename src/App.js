767358e64ffec8acfff0db8fc6df54c5425a2dc7
dragunovlife <dragunovlife@gmail.com>
2020-03-05 20:58:43 +0300

    Applied grid in the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import Header from './Header'
-import Technologies from './Technologies'
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      Social Network
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+
+    <header className='header'>
+      <img src="https://avatars.mds.yandex.net/get-pdb/236760/db1cde15-b660-4984-a34e-570f1fc48587/s1200" />
+    </header>
+
+    <nav className="nav">
+      <div>
+        <a>Profile</a>
+      </div>
+      <div>
+        <a>Messages</a>
+      </div>
+      <div>
+        <a>News</a>
+      </div>
+      <div>
+        <a>Music</a>
+      </div>
+      <div>
+        <a>Settings</a>
+      </div>
+    </nav>
+
+    <div className="content">
+        <div>
+        <img src="https://avatars.mds.yandex.net/get-pdb/1736242/a7f9c41c-4eb1-4870-8711-0e241ffed085/s1200" />
+        </div>
+
+        <div>
+          ava+description
+        </div>
+
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+
+    </div>
+
     </div>
   );
 }
