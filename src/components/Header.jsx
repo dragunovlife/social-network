ba17a7b5addff035d00a8b11d34c0c255e29cbe5
dragunovlife <dragunovlife@gmail.com>
2020-03-09 03:49:04 +0300

    Used css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
-  return <header className='header'>
+  return <header className={classes.header}>
     <img src="https://avatars.mds.yandex.net/get-pdb/236760/db1cde15-b660-4984-a34e-570f1fc48587/s1200" />
   </header>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
-  return  <div className="content">
-  <div>
-  <img src="https://avatars.mds.yandex.net/get-pdb/1736242/a7f9c41c-4eb1-4870-8711-0e241ffed085/s1200" />
-  </div>
-
-  <div>
-    ava+description
-  </div>
-
-  <div>
-    My posts
+  return <div className={classes.content}>
     <div>
-      New post
+      <img src="https://avatars.mds.yandex.net/get-pdb/1736242/a7f9c41c-4eb1-4870-8711-0e241ffed085/s1200" />
     </div>
+
     <div>
-      <div>post 1</div>
-      <div>post 2</div>
-    </div>
+      ava+description
   </div>
 
-</div>
+    <div>
+      My posts
+    <div>
+        New post
+    </div>
+      <div className={classes.posts}>
+        <div className={classes.item}>
+          post 1
+      </div>
+        <div className={classes.item}>
+          post 2
+      </div>
+      </div>
+    </div>
+
+  </div>
 
 }
 
