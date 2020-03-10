17fe7fb8315593a9f6c7fbf080b3fd37eae8cb65
dragunovlife <dragunovlife@gmail.com>
2020-03-10 06:48:51 +0300

    Updated css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,13 @@ import classes from './Header.module.css';
 const Header = () => {
   return <header className={classes.header}>
     <img src="https://avatars.mds.yandex.net/get-pdb/236760/db1cde15-b660-4984-a34e-570f1fc48587/s1200" />
+    <ul>
+      <li>Home</li>
+      <li>Discussion</li>
+      <li>Weather</li>
+      <li>Pages</li>
+      <li>Blog</li>
+    </ul>
   </header>
 }
 
