325d34390cc51f0f7dcfed56affba3e3d1f1f8e0
dragunovlife <dragunovlife@gmail.com>
2020-03-14 01:14:18 +0300

    Page layout dialogues

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Sidebar.module.css';
+import {NavLink} from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+  <div className={classes.side}>
+    Sidebar
+  </div>
+  )
+}
+
+export default Sidebar;
